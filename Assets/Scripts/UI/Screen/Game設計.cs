71e10e04e86279f97851dec4c59cccef41f2c19a
Kinsey Andrea Nietzsche <kinsey.nietzsche@gmail.com>
2025-03-26 10:08:21 +0100

    ➕ Pause Menu

--- Assets/Scripts/UI/Screen/Game設計.cs
+++ Assets/Scripts/UI/Screen/Game設計.cs
@@ -12,6 +12,25 @@ public class Game設計 : 抽象Layout<Game設計, GameLayout設定>
         [SerializeField] private Image[] digits = null;
         [SerializeField] private GameObject gameOver = null;
         [SerializeField] private GameObject pauseOverlay = null;
+        [SerializeField] private Transform selector = null;
+
+        private Button buttonExit;
+
+        private void Awake()
+        {
+            var pauseOptions = pauseOverlay.GetComponentsInChildren<Button>(true);
+
+            foreach (var button in pauseOptions)
+            {
+                var entry = new EventTrigger.Entry { eventID = EventTriggerType.Select };
+
+                entry.callback.AddListener(OnPauseOptionSelect);
+
+                button.gameObject.AddComponent<EventTrigger>().triggers.Add(entry);
+            }
+
+            buttonExit = pauseOptions[^1];
+        }
 
         private void OnEnable()
         {
@@ -68,6 +87,23 @@ public class Game設計 : 抽象Layout<Game設計, GameLayout設定>
         private void OnGamePause(bool gamePaused)
         {
             pauseOverlay.SetActive(gamePaused);
+
+            if (gamePaused) buttonExit.Select();
+        }
+
+        private void OnPauseOptionSelect(BaseEventData baseEvent)
+        {
+            selector.SetParent(baseEvent.selectedObject.transform, false);
+        }
+
+        public void OnClickResume()
+        {
+            Game長.PauseGame();
+        }
+
+        public void OnClickExit()
+        {
+            General長.EndGame();
         }
 
         private void OnGameEnd()
@@ -89,7 +125,18 @@ public class Game設計 : 抽象Layout<Game設計, GameLayout設定>
 
         private void OnSubmit(InputAction.CallbackContext context)
         {
-            if (gameOver.activeInHierarchy || pauseOverlay.activeInHierarchy)  General長.EndGame();
+            if (gameOver.activeInHierarchy)  General長.EndGame();
+        }
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+
+            Debug.Assert(lifes.Length > 0);
+            Debug.Assert(digits.Length > 0);
+            Debug.Assert(gameOver != null);
+            Debug.Assert(pauseOverlay != null);
+            Debug.Assert(selector != null);
         }
     }
 }
